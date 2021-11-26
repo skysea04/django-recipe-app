9834407e2ebf82efc86139c3bfc7479f78e6a6fb
邱宇軒 <skysea@qiuyuxuandeMacBook-Air.local>
2021-11-27 01:06:33 +0800

    Add adin to support custom user model

--- app/core/tests.py
+++ app/core/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 # Create your tests here.
 
 class ModelTests(TestCase):
@@ -36,3 +37,25 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email = 'admin@skysea.com',
+            password = 'test123456'
+        )
+
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='test@skysea.com',
+            password = 'test123456',
+            name = 'test user full name'
+        )
+
+    def test_users_listed(self):
+        url = reverse('admin:core_user_changelist')
+        res = self.client.get(url)
+        
+        self.assertContains(res, self.user.name)
+        self.assertContains(res, self.admin_user.email)
